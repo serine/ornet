3a2990441b319af8c4a24bdb9cb9818a0f5898ce
Adurden <durden4th@gmail.com>
2019-04-17 11:09:58 -0400

    Fixed Style issues in segmentation script

--- src/extract_cells.py
+++ src/extract_cells.py
@@ -5,22 +5,22 @@ initial masking image of the first frame. It will then create contours for
 each subsequent frame and use those contours to mask out each cell. It will
 then save each cell in their own video
 '''
-#Author : Andrew Durden
+# Author : Andrew Durden
 import argparse
-from functools import partial
 import os
+import random
+from functools import partial
 
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 import imageio
 import numpy as np
-import random
+from matplotlib import pyplot as plt
 
-def extract_cells(vidfile, maskfile, show_video = False):
+
+def extract_cells(vidfile, maskfile, show_video=False):
     """
-    reads a video file and initial masks and returns a set of frames for each cell
-
+    reads video file and initial masks and returns a set of frames for each
+    cell
 
     Parameters
     ----------
@@ -40,145 +40,163 @@ def extract_cells(vidfile, maskfile, show_video = False):
         plt.imshow(im)
         plt.show()
 
-    #vf = cv2.VideoCapture(vidfile)    #OpenCV videocapture object
     vf = imageio.get_reader(vidfile)
 
-    # obtaining the frame dimensions
-    #X = int(vf.get(3)) #vf.get returns specific properties, 3 is width 4 is height
-    #Y = int(vf.get(4))
-
+    # declare variables
     frameNum = 0
-    number_of_segments = len(np.unique(im))-1 #defines number of segs from vtk
+    number_of_segments = len(np.unique(im))-1  # define number of segs from vtk
     print(number_of_segments)
     outs = []
     ims = list()
     masks = list()
-    colors = list() #the colors to make the contours in the output video?
-    contours = list() #holds contours
+    colors = list()
+    contours = list()
     dilates = list()
     comparisons = list()
 
-    kernel = np.ones((17,17), np.uint8) #kernel for opening
-    kernel2 = np.ones((3,3),np.uint8) #kernel for dilation
-    font = cv2.FONT_HERSHEY_SIMPLEX #font for frame count
+    kernel = np.ones((17, 17), np.uint8)  # kernel for opening
+    kernel2 = np.ones((3, 3), np.uint8)  # kernel for dilation
+    font = cv2.FONT_HERSHEY_SIMPLEX  # font for frame count when show_video
 
-    for cols in range (number_of_segments): #creates random colors to use for the outlines
-        colors.append((random.randint(1,255), random.randint(0,255), random.randint(1,255)))
+    # determine random colors to use for the outlines in show_video
+    for cols in range(number_of_segments):
+        colors.append((random.randint(1, 255), random.randint(0, 255),
+                       random.randint(1, 255)))
 
-    for i in range ( number_of_segments ): #separates each mask from the vtk and lists them
-            masks.append( im != i + 1 )
+    # separates each mask from the vtk and lists them
+    for i in range(number_of_segments):
+            masks.append(im != i + 1)
 
-    for frameNum, frame in enumerate(vf):#while( vf.isOpened() ):
-        #for i in range(3):#skips to every third frame
-        #frameNum += 1 #advance through frames
-        #if(frameNum == 1):
-            #for i in range(13210):
-                #frameNum += 1
-                #ret, frame = vf.read()
-                #print(vf.isOpened())
-        for i in range (number_of_segments): #adds a copy of the current frame for each segment
+    # for each frame
+    for frameNum, frame in enumerate(vf):
+        # create a copy of the current frame for each cell
+        for i in range(number_of_segments):
             ims.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
-        for i in range (number_of_segments): #blacks out all that isn't in the initial mask for each segment
+        # mask out all that isn't in the initial/previous mask for each segment
+        for i in range(number_of_segments):
             if(frameNum == 0):
                 ims[i][masks[i]] = 0
+            # use previous contour for next frame
             else:
-                ims[i][dilates[i]!=255] = 0 #uses previous dilation for next frame
+                ims[i][dilates[i] != 255] = 0
 
+        # delete previous masks once used for memory
         del dilates[:]
+
+        # for each use threhold based segmentation to make strict contour
         for i in range(number_of_segments):
-            ret , ims[i] = cv2.threshold( ims[i], 3, 255, cv2.THRESH_BINARY_INV)
-            temp = cv2.morphologyEx( ims[i], cv2.MORPH_OPEN, kernel, iterations = 3)
+            ret, ims[i] = cv2.threshold(ims[i], 3, 255, cv2.THRESH_BINARY_INV)
+            temp = cv2.morphologyEx(ims[i], cv2.MORPH_OPEN, kernel,
+                                    iterations=3)
             temp = cv2.bitwise_not(temp)
             dilates.append(temp)
 
-
-        #each segment dilates
-        #if two segments overlap cut the overlapping portions from both (first one to cover a place gets it...)
-        if(frameNum%100 == 1 or frameNum <20): #every 100 frames do full pairwise comparisons and save which were adjacent
-            for i in range (5):
+        """
+        Iterative dilation for each cell with added overlap detection. For
+        runtime a full pairwise overlap detection is done for each cell for the
+        first 20 frames. During these frames, a lookup list us made of cells
+        which overlap. After the 20 frames only the pairs in the lookup are
+        checked, except every 100 frames where a full pairwise check is done
+        again and the lookup is updated.
+        """
+        # full pairwise overlap detection
+        if(frameNum % 100 == 1 or frameNum < 20):
+            for i in range(5):
                 del comparisons[:]
-                for d in range (number_of_segments):
-                    dilates[d] = cv2.dilate(dilates[d],kernel2,iterations=2)
-                for s in range (number_of_segments): #tests each segment with every other segment, slow
+                for d in range(number_of_segments):
+                    dilates[d] = cv2.dilate(dilates[d], kernel2, iterations=2)
+                for s in range(number_of_segments):
                     newcomps = list()
                     for t in range(s+1, number_of_segments):
-                        b_and = cv2.bitwise_and(dilates[s],dilates[t])
+                        b_and = cv2.bitwise_and(dilates[s], dilates[t])
                         if(len(np.unique(b_and)) > 1):
                             newcomps.append(t)
-                            dilates[s] = cv2.bitwise_xor(dilates[s],b_and)
-                            dilates[t] = cv2.bitwise_xor(dilates[t],b_and)
+                            dilates[s] = cv2.bitwise_xor(dilates[s], b_and)
+                            dilates[t] = cv2.bitwise_xor(dilates[t], b_and)
                     comparisons.append(newcomps)
-
-        else: #every other frame only compare those that overlapped previously
-            for i in range (5):
-                for d in range (number_of_segments):
-                    dilates[d] = cv2.dilate(dilates[d],kernel2,iterations=2)
-                for s in range (number_of_segments):
+        # lookup list based overlap detection
+        else:
+            for i in range(5):
+                for d in range(number_of_segments):
+                    dilates[d] = cv2.dilate(dilates[d], kernel2, iterations=2)
+                for s in range(number_of_segments):
                     for t in comparisons[s]:
-                        b_and = cv2.bitwise_and(dilates[s],dilates[t])
+                        b_and = cv2.bitwise_and(dilates[s], dilates[t])
                         if(len(np.unique(b_and)) > 1):
-                            dilates[s] = cv2.bitwise_xor(dilates[s],b_and)
-                            dilates[t] = cv2.bitwise_xor(dilates[t],b_and)
+                            dilates[s] = cv2.bitwise_xor(dilates[s], b_and)
+                            dilates[t] = cv2.bitwise_xor(dilates[t], b_and)
 
-        for conts in range (number_of_segments): #bulds the contours and draws them onto the frame
-            contours.append(cv2.findContours( dilates[conts], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1])
-            cv2.drawContours( frame, contours[conts], -1, colors[conts], 2)
-            if (conts == number_of_segments -1 and show_video) : #prints contours frame by frame
-                cv2.putText(frame, 'Frame # '+ str(frameNum), (10, 40), font, 0.5, (0, 255, 50), 1)
-                cv2.imshow( "Keypoints2", frame)
+        # draws contours onto video for show_video
+        for conts in range(number_of_segments):
+            contours.append(cv2.findContours(dilates[conts], cv2.RETR_TREE,
+                                             cv2.CHAIN_APPROX_SIMPLE)[1])
+            cv2.drawContours(frame, contours[conts], -1, colors[conts], 2)
+            if (conts == number_of_segments - 1 and show_video):
+                cv2.putText(frame, 'Frame # ' + str(frameNum), (10, 40), font,
+                            0.5, (0, 255, 50), 1)
+                cv2.imshow("Keypoints2", frame)
                 k = cv2.waitKey(10)
                 if (k == 27):
                     break
 
+        # combines final contours into single frame of masks
         frameMask = np.zeros_like(dilates[0])
         for i in range(number_of_segments):
             frameMask[dilates[i] == 255] = i+1
         outs.append(frameMask)
 
+        # free up resources
         del ims[:]
         del contours[:]
         del masks[:]
 
+    # advanced video frames in show_video
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    # returns list of mask frames
     return outs
 
 
 if __name__ == '__main__':
     cwd = os.getcwd()
     parser = argparse.ArgumentParser(
-        description = "Read Video File and VTK File and creates videos of each cell in the video",
-        add_help = "How to use", prog = 'extract_cells.py <args>')
+        description="Read Video File and VTK File and creates videos of each"
+                    "cell in the video",
+        add_help="How to use", prog='extract_cells.py <args>')
 
-    #Required args
-    parser.add_argument("-i","--input", required = True,
-        help = "The path to a single video file")
+    # Required args
+    parser.add_argument("-i", "--input", required=True,
+                        help="The path to a single video file")
 
-    parser.add_argument("-m","--masks", required = True,
-        help = "The path to a single vtk file containing first frame masks")
+    parser.add_argument("-m", "--masks", required=True,
+                        help="The path to a single vtk file containing"
+                             " first frame masks")
 
-    #Optional args
-    parser.add_argument("-o","--output", default = os.path.join(cwd, "single_cell_videos"),
-        help = "Destination path for output videos. [Default: cwd/single_cell_videos]")
+    # Optional args
+    parser.add_argument("-o", "--output",
+                        default=os.path.join(cwd, "single_cell_videos"),
+                        help="Destination path for output videos."
+                             " [Default: cwd/single_cell_videos]")
 
-    parser.add_argument("-s","--showvid", action = "store_true",
-        help = "If set, each frame with contours is drawn during processing. [Default: False]")
-
+    parser.add_argument("-s", "--showvid", action="store_true",
+                        help="If set, each frame with contours is drawn during"
+                             " processing. [Default: False]")
 
     args = vars(parser.parse_args())
 
+    # create output directory
     if not os.path.exists(args['output']):
         os.mkdir(args['output'])
 
     vidpath = args['input']
 
-    out = extract_cells(vidfile = args['input'], maskfile = args['masks'], show_video = args['showvid'])
+    out = extract_cells(vidfile=args['input'], maskfile=args['masks'],
+                        show_video=args['showvid'])
     fname = vidpath.split("/")[-1].split(".")[0]
-    #print(len(them[0]),len(them[1]))
-    #write the files out
 
+    # write the files as npy files
     fname = "{}.npy".format(fname+'MASKS')
     outfile = os.path.join(args['output'], fname)
     np.save(outfile, out)
